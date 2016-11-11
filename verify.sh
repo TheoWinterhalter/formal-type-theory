87366589c4de1c4f01f5c7a9f2366d5e2d06b3bc
Andrej Bauer <Andrej.Bauer@andrej.com>
2016-11-11 16:38:45 +0100

    A bash script to check rules are in order.

---
+++ verify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The script checks that rules.tex, sanity.tex and tt.tex have the same rules
+# in the same order.
+
+sanityRules=$(sed -n 's/^.*section\*{Rule {\\rl\([A-Za-z]*\)}.*$/\1/p' sanity.tex)
+rulesRules=$(sed -n 's/^.*\\rl\([A-Za-z]*\)}.*$/\1/p' rules.tex)
+ttRules=$(sed -n 's/^.*\\show\([A-Za-z]*\)}.*$/\1/p' tt.tex)
+
+echo "Checking sanity.tex against rules.tex:"
+diff <(echo "$rulesRules") <(echo "$sanityRules")
+
+echo "Checking tt.tex against rules.tex:"
+diff <(echo "$rulesRules") <(echo "$ttRules")
+
+
+
+
