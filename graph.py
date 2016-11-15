a9ad5c6350077f2284ab2c44a645f40750936cbf
Andrej Bauer <Andrej.Bauer@andrej.com>
2016-11-15 08:54:22 +0100

    Improve generation of the sanity graph.
    * Rather than linking to all rules mentioned in the clause, we only link
      to the explicitly given rule that is the last one used in the
      generated derivation.
    * The script `graph.py` now checks for cycles.
    * It also generates the `sanity.dot.pdf` file.

--- graph.py
+++ graph.py
@@ -1,25 +1,31 @@
 #!/bin/bash
 import re
+import subprocess
 
 with open("sanity.tex", "r") as f:
-    print ("digraph TypeTheory {")
-    src = None
-    trg = None
-    for line in f:
-        m = re.search(r'\\show(\w+)', line)
-        if m:
-            if src is not None:
-                print ('{0} -> {{ {1} }}; \n'.format(src, " ".join(trg)))
-            src = m.group(1)
-            trg = set([])
-        else:
-            m = re.search(r'\\subsubsection*', line);
-            if not m:
-                m = re.search(r'\\rl(\w+)', line);
-                if m:
-                    if src:
-                        trg.add(m.group(1))
-    print("}")
+    with open("sanity.dot", "w") as g:
+        g.write("digraph SanityGraph {")
+        src = None
+        trg = None
+        for line in f:
+            m = re.search(r'\\show(\w+)', line)
+            if m:
+                if src is not None:
+                    g.write('{0} -> {{ {1} }}; \n'.format(src, " ".join(trg)))
+                src = m.group(1)
+                trg = set([])
+            else:
+                m = re.search(r'^% ENDS WITH (\w+)', line);
+                if m and src:
+                    trg.add(m.group(1))
+        g.write("}")
+
+returnCode = subprocess.call(["acyclic", "-n", "sanity.dot"])
+if returnCode != 0:
+    print ("!!! THE GRAPH IS NOT ACYCLIC !!!")
+
+subprocess.call(["dot", "-Tpdf", "-O", "sanity.dot"])
+
 
 # Then you do:
 # python graph.py > foo.dot
