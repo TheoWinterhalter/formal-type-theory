cf72cd90f09126ff543a08962ee62b799d38258b
Théo Winterhalter <isheeft@gmail.com>
2016-11-15 18:53:25 +0100

    More complete graph with IH
    The idea is that fullgraph actually cares for which kind of derivation
    can be returned.
    We would have to do the same for premises, but it is already nice to
    remark that this version still generates a DAG!

--- fullgraph.py
+++ fullgraph.py
@@ -2,18 +2,28 @@
 import re
 import subprocess
 
-rules = set([])
+ruledic = {}
+cat = None
+
+def union(a,b): return a|b
 
 with open("rules.tex", "r") as r:
     for line in r:
-        m = re.search(r'\\rl(\w+)', line)
+        m = re.search(r'\%\%\%\% (.+)', line)
         if m:
-            rule = m.group(1)
-            if rule in rules:
-                print ('!!! DUPLICATE RULE {0} !!!'.format(rule))
-            rules.add(rule)
+            cat = m.group(1)
+            ruledic[cat] = set([])
+        elif cat is not None:
+            m = re.search(r'\\rl(\w+)', line)
+            if m:
+                rule = m.group(1)
+                if rule in reduce(union, ruledic.values()):
+                    print ('!!! DUPLICATE RULE {0} !!!'.format(rule))
+                ruledic[cat].add(rule)
 
+rules = reduce(union, ruledic.values())
 print ("{0} rules loaded.".format(len(rules)))
+
 
 with open("sanity.tex", "r") as f:
     with open("sanity.dot", "w") as g:
@@ -30,12 +40,17 @@ with open("sanity.tex", "r") as f:
                     print ('!!! UNKNOWN SOURCE RULE {0} !!!'.format(src))
                 trg = set([])
             else:
-                m = re.search(r'^% ENDS WITH (\w+)', line);
+                m = re.search(r'^% ENDS WITH (.+)', line);
                 if m and src:
                     t = m.group(1)
-                    trg.add(t)
-                    if t not in rules and t not in ['IH', 'Premise', 'Inversion']:
-                        print ('!!! UNKNOWN TARGET RULE {0} !!!'.format(t))
+
+                    mIH = re.search(r'IH (.+)', t)
+                    if mIH and mIH.group(1) in ruledic.keys():
+                        trg |= ruledic[mIH.group(1)]
+                    else:
+                        trg.add(t)
+                        if t not in rules and t not in ['IH', 'Premise', 'Inversion']:
+                            print ('!!! UNKNOWN TARGET RULE {0} !!!'.format(t))
 
         g.write("}")
 
@@ -48,4 +63,5 @@ subprocess.call(["dot", "-Tpdf", "-O", "sanity.dot"])
 print ("Wrote sanity.dot.pdf")
 
 # Then you do:
-# python graph.py
+# python graph.py > foo.dot
+# dot -Tpdf foo.dot > foo.pdf
