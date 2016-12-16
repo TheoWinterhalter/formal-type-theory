833056d15207b409451c03665cc48586f5dd1a5c
Théo Winterhalter <isheeft@gmail.com>
2016-12-16 16:16:44 +0100

    First attempt at coercion definition

--- formalization/ctt.v
+++ formalization/ctt.v
@@ -1,5 +1,58 @@
 (* The intermediate type theory with coercions
    (CTT stands for Coercive Type Theory). *)
+Require itt.
+
+
+(* Coercions between contexts. *)
+Structure contextCoercion {G D : itt.context} :=
+  { ctx_bij : itt.substitution
+  ; ctx_inv : itt.substitution
+  ; ctx_bij_ty : itt.issubst ctx_bij G D
+  ; ctx_inv_ty : itt.issubst ctx_inv D G
+  ; ctx_coh :
+      forall A u,
+        itt.isterm D u A ->
+        { p : itt.term
+        & itt.isterm D p (itt.Id A (itt.subst (itt.subst u ctx_bij) ctx_inv) u)
+        }
+  }.
+
+(* Coercions between types, over a context coercion. *)
+Structure typeCoercion {G D : itt.context} (c : @contextCoercion G D)
+                       (A B : itt.type) :=
+  { ty_bij : itt.term
+  ; ty_inv : itt.term
+  ; ty_bij_ty :
+      itt.isterm G
+                 ty_bij
+                 (itt.Prod
+                    A
+                    (itt.Subst (itt.Subst B (ctx_bij c)) (itt.sbweak G A)))
+  ; ty_inv_ty :
+      itt.isterm D
+                 ty_inv
+                 (itt.Prod
+                    B
+                    (itt.Subst (itt.Subst A (ctx_inv c)) (itt.sbweak D B)))
+  ; ty_coh :
+      forall x,
+        itt.isterm G x A ->
+        { p : itt.term
+        & itt.isterm G p
+                     (itt.Id A
+                       (itt.app
+                          (itt.subst ty_inv (ctx_bij c))
+                          (itt.Subst B (ctx_bij c))
+                          (itt.Subst A (itt.sbweak G (itt.Subst B (ctx_bij c))))
+                          (itt.app
+                             ty_bij
+                             A
+                             (itt.Subst (itt.Subst B (ctx_bij c))
+                                        (itt.sbweak G A))
+                             x))
+                       x)
+        }
+  }.
 
 
 Parameter typeCoerce : Type.
