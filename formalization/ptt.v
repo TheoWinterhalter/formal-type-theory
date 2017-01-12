57ed720a8707c89d99993ca065accca6c7a8f745
Théo Winterhalter <isheeft@gmail.com>
2017-01-12 12:20:12 +0100

    More admissibility—Need for better magic

--- formalization/ptt.v
+++ formalization/ptt.v
@@ -1819,7 +1819,7 @@ Proof.
   ].
 Defined.
 
-Lemma substWeakZero :
+Lemma EqTyWeakZero :
   forall {G A B u},
     isctx G ->
     istype G A ->
@@ -1830,6 +1830,48 @@ Proof.
   intros.
   gocompsubst. eapply EqTySym ; try magic. apply compWeakZero ; magic.
 Defined.
+
+Lemma EqTyShiftZero :
+  forall {G D A B v sbs},
+    issubst sbs G D ->
+    istype D A ->
+    istype (ctxextend D A) B ->
+    isterm D v A ->
+    isctx G ->
+    isctx D ->
+    eqtype
+      G
+      (Subst (Subst B (sbshift G A sbs)) (sbzero G (Subst A sbs) (subst v sbs)))
+      (Subst (Subst B (sbzero D A v)) sbs).
+Proof.
+  intros. gocompsubst. gocompsubst.
+  eapply myCongTySubst ; magic.
+  Unshelve. magic.
+Defined.
+
+Lemma EqTyCongZero :
+  forall {G A1 A2 B1 B2 u1 u2},
+    isctx G ->
+    eqtype G A1 B1 ->
+    eqterm G u1 u2 A1 ->
+    eqtype (ctxextend G A1) A2 B2 ->
+    istype (ctxextend G A1) A2 ->
+    istype (ctxextend G A1) B2 ->
+    eqtype G
+           (Subst A2 (sbzero G A1 u1))
+           (Subst B2 (sbzero G B1 u2)).
+Proof.
+  intros.
+  eapply myCongTySubst.
+  - admit. (* We need to strengthen magic. *)
+  - eassumption. (* Same here... *)
+  - magic.
+  - magic.
+  - magic.
+  - magic.
+  - admit. (* Weird it doesn't solve this. *)
+  - admit.
+Admitted.
 
 Definition sane_issubst sbs G D :
   issubst sbs G D -> isctx G * isctx D.
@@ -1946,7 +1988,7 @@ Proof.
       assert (eqtype G A (Subst A (sbcomp (sbweak G A) (sbzero G A v)))).
       { apply compWeakZero ; assumption. }
       assert (eqtype G A (Subst (Subst A (sbweak G A)) (sbzero G A v))).
-      { apply substWeakZero ; assumption. }
+      { apply EqTyWeakZero ; assumption. }
       gopushsubst. apply CongId ; try magic.
       + eapply myTermTyConv.
         * eassumption.
@@ -2492,7 +2534,7 @@ Proof.
             + now apply SubstZero.
             + now apply TermVarSucc.
           - apply EqTySym ; try magic.
-            apply substWeakZero ; magic.
+            apply EqTyWeakZero ; magic.
         }
       - { assumption. }
     }
