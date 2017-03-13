89910d7de30da7b1a27a54ccf93bddd6f2d7f223
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-03-13 17:19:46 +0100

    Fancier and broken coercions.
    We're trying to be more precise about what the coercions are
    actually doing. It's not super easy.

--- formalization/coerce.v
+++ formalization/coerce.v
@@ -7,11 +7,22 @@ Axiom cheating : forall A : Type, A.
 Definition todo {A} := cheating A.
 
 Inductive ctxcoe : Type :=
-  | ctxcoe_identity : ctxcoe.
+  | ctxcoe_identity : ctxcoe
 
-Inductive tycoe : Type :=
+  | ctxcoe_ctxextend :
+      forall c1 : ctxcoe, (* c1 : G ~~~> D *)
+      forall c2 : tycoe,  (* for some G ⊢ A and D ⊢ B:
+                             D ⊢ c2 : (act_type c1 A) ~~~> B *)
+        ctxcoe (* G, A ~~~> D, B *)
+
+with tycoe : Type :=
   | tycoe_identity : tycoe
-  | tycoe_prod : tycoe -> tycoe -> tycoe
+  | tycoe_prod :
+      forall (A1 B1 : type) (* G, A1 ⊢ B1 *)
+             (A2 B2 : type) (* G, A2 ⊢ B2 *)
+             (cA : tycoe) (* G ⊢ cA : A1 ~~~> A2 *)
+             (cB : tycoe), (* G, A2 ⊢ (act_type (ctxcoe_ctxextend ctxcoe_identity c1) B1) ~~~> B2 *)
+        tycoe (* Prod A1 B1 ~~~> Prod A2 B2 *)
   | tycoe_id : tycoe -> termcoe -> termcoe -> tycoe
 
 with termcoe : Type :=
@@ -21,24 +32,27 @@ with termcoe : Type :=
 
 (* Computation of inverses of coercions *)
 
-Definition inv_ctxcoe (crc : ctxcoe) : ctxcoe :=
+Fixpoint inv_ctxcoe (crc : ctxcoe) : ctxcoe :=
   match crc with
   | ctxcoe_identity => ctxcoe_identity
-  end.
+  | ctxcoe_ctxextend c1 c2 => ctxcoe_ctxextend (inv_tycoe c1)
+  end
 
-Fixpoint inv_termcoe (crtt : termcoe) : termcoe :=
+with inv_tycoe (crt : tycoe) : tycoe :=
+  match crt with
+  | tycoe_identity => tycoe_identity
+  | tycoe_prod A1 B1 A2 B2 cA cB =>
+    tycoe_prod A2 B2 A1 B1 (inv_tycoe cA) (inv_tycoe cB)
+  | tycoe_id cA cu cv => tycoe_id (inv_tycoe cA) (inv_termcoe cu) (inv_termcoe cv)
+  end
+
+with inv_termcoe (crtt : termcoe) : termcoe :=
   match crtt with
   | termcoe_identity => termcoe_identity
   | termcoe_reflection A u v p =>
     termcoe_reflection A v u (j A u (Id A (var 1) u) (refl A u) v p)
   end.
 
-Fixpoint inv_tycoe (crt : tycoe) : tycoe :=
-  match crt with
-  | tycoe_identity => tycoe_identity
-  | tycoe_prod cA cB => tycoe_prod (inv_tycoe cA) (inv_tycoe cB)
-  | tycoe_id cA cu cv => tycoe_id (inv_tycoe cA) (inv_termcoe cu) (inv_termcoe cv)
-  end.
 
 
 (* Action of coercions on expressions *)
@@ -69,7 +83,20 @@ Fixpoint act_term_ctx (crc : ctxcoe) (u : term) : term :=
 Fixpoint act_term_type (crc : tycoe) (u : term) : term :=
   match crc with
   | tycoe_identity => u
-  | tycoe_prod cA cB => todo (* Problem with the definition of cB! *)
+  | tycoe_prod A1 B1 A2 B2 cA cB =>
+    (* The situation:
+       G ⊢ A1
+       G, A1 ⊢ B1
+       G ⊢ A2
+       G, A2 ⊢ B2
+       G ⊢ cA : A1 ⇒ A2
+       G, A2 ⊢ cB : (act_(ctxextend G cA) B1) ⇒ B2
+     *)
+    lam
+      A2
+      B2
+      (act_term_type cB (app u A1 B1 ()))
+
   | tycoe_id cA cu cv => todo (* I'm a bit lost *)
   end.
 
