e6f68886813d671689199ebe434122abc55bdc65
Théo Winterhalter <isheeft@gmail.com>
2017-03-19 14:26:49 +0100

    Starting translation (problems with type classes)

--- formalization/negfunext.v
+++ formalization/negfunext.v
@@ -17,8 +17,7 @@ Module Stt.
   Local Instance hasPrecond : config.Precond
     := {| config.precondFlag := config.Yes |}.
   Context `{configReflection : config.Reflection}.
-  Local Instance hasSimpleProducts : config.SimpleProducts
-    := {| config.simpleproductsFlag := config.No |}.
+  Context `{ConfigSimpleProducts : config.SimpleProducts}.
 
   Definition isctx   := isctx.
   Definition issubst := issubst.
@@ -56,3 +55,235 @@ Module Ttt.
   End Ttt.
 
 End Ttt.
+
+Section Translation.
+
+Context `{configReflection : config.Reflection}.
+Context `{configSimpleProducts : config.SimpleProducts}.
+
+Axiom cheating : forall A : Type, A.
+Ltac todo := apply cheating.
+
+Fixpoint trans_ctx (G : context) : context :=
+  match G with
+  | ctxempty => ctxempty
+  | ctxextend G A => ctxextend (trans_ctx G) (trans_type A)
+  end
+
+with trans_type (A : type) : type :=
+  match A with
+  | Prod A B => SimProd (Prod (trans_type A) (trans_type B)) Bool
+  | Id A u v => Id (trans_type A) (trans_term u) (trans_term v)
+  | Subst A sbs => Subst (trans_type A) (trans_subst sbs)
+  | Empty => Empty
+  | Unit => Unit
+  | Bool => Bool
+  | SimProd A B => SimProd (trans_type A) (trans_type B)
+  end
+
+with trans_term (t : term) : term :=
+  match t with
+  | var k => var k
+  | lam A B u =>
+    pair (Prod (trans_type A) (trans_type B))
+         Bool
+         (lam (trans_type A) (trans_type B) (trans_term u))
+         true
+  | app u A B v =>
+    app (proj1 (Prod (trans_type A) (trans_type B)) Bool (trans_term u))
+        (trans_type A)
+        (trans_type B)
+        (trans_term v)
+  | refl A u => refl (trans_type A) (trans_term u)
+  | j A u C w v p =>
+    j (trans_type A)
+      (trans_term u)
+      (trans_type C)
+      (trans_term w)
+      (trans_term v)
+      (trans_term p)
+  | subst u sbs => subst (trans_term u) (trans_subst sbs)
+  | exfalso A u => exfalso (trans_type A) (trans_term u)
+  | unit => unit
+  | true => true
+  | false => false
+  | cond C u v w =>
+    cond (trans_type C) (trans_term u) (trans_term v) (trans_term w)
+  | pair A B u v =>
+    pair (trans_type A) (trans_type B) (trans_term u) (trans_term v)
+  | proj1 A B p =>
+    proj1 (trans_type A) (trans_type B) (trans_term p)
+  | proj2 A B p =>
+    proj2 (trans_type A) (trans_type B) (trans_term p)
+  end
+
+with trans_subst (sbs : substitution) : substitution :=
+  match sbs with
+  | sbzero A u => sbzero (trans_type A) (trans_term u)
+  | sbweak A => sbweak (trans_type A)
+  | sbshift A sbs => sbshift (trans_type A) (trans_subst sbs)
+  | sbid => sbid
+  | sbcomp sbs sbt => sbcomp (trans_subst sbs) (trans_subst sbt)
+  end.
+
+Ltac ih :=
+  match goal with
+  | trans_isctx :
+      forall G,
+        Stt.isctx G ->
+        Ttt.isctx (trans_ctx G)
+    |- isctx (trans_ctx ?G) =>
+    now apply (trans_isctx G)
+  | trans_istype :
+      forall G A,
+        Stt.istype G A ->
+        Ttt.istype (trans_ctx G) (trans_type A)
+    |- istype (trans_ctx ?G) (trans_type ?A) =>
+    now apply (trans_istype G A)
+  | trans_isterm :
+      forall G u A,
+        Stt.isterm G u A ->
+        Ttt.isterm (trans_ctx G) (trans_term u) (trans_type A)
+    |- isterm (trans_ctx ?G) (trans_term ?u) (trans_type ?A) =>
+    now apply (trans_isterm G u A)
+  | trans_issubst :
+      forall sbs G D,
+        Stt.issubst sbs G D ->
+        Ttt.issubst (trans_subst sbs) (trans_ctx G) (trans_ctx D)
+    |- issubst (trans_subst ?sbs) (trans_ctx ?G) (trans_ctx ?D) =>
+    now apply (trans_issubst sbs G D)
+  | trans_eqctx :
+      forall G D,
+        Stt.eqctx G D ->
+        Ttt.eqctx (trans_ctx G) (trans_ctx D)
+    |- eqctx (trans_ctx ?G) (trans_ctx ?D) =>
+    now apply (trans_eqctx G D)
+  | trans_eqtype :
+      forall G A B,
+        Stt.eqtype G A B ->
+        Ttt.eqtype (trans_ctx G) (trans_type A) (trans_type B)
+    |- eqtype (trans_ctx ?G) (trans_type ?A) (trans_type ?B) =>
+    now apply (trans_eqtype G A B)
+  | trans_eqterm :
+      forall G u v A,
+        Stt.eqterm G u v A ->
+        Ttt.eqterm (trans_ctx G) (trans_term u) (trans_term v) (trans_type A)
+    |- eqterm (trans_ctx ?G) (trans_term ?u) (trans_term ?v) (trans_type ?A) =>
+    now apply (trans_eqterm G u v A)
+  | trans_eqsubst :
+      forall sbs sbt G D,
+        Stt.eqsubst sbs sbt G D ->
+        Ttt.eqsubst (trans_subst sbs)
+                    (trans_subst sbt)
+                    (trans_ctx G)
+                    (trans_ctx D)
+    |- eqsubst (trans_subst ?sbs)
+              (trans_subst ?sbt)
+              (trans_ctx ?G)
+              (trans_ctx ?D) =>
+    now apply (trans_eqsubst sbs sbt G D)
+  end.
+
+Fixpoint trans_isctx {G} (H : Stt.isctx G) {struct H} :
+  Ttt.isctx (trans_ctx G)
+
+with trans_istype {G A} (H : Stt.istype G A) {struct H} :
+  Ttt.istype (trans_ctx G) (trans_type A)
+
+with trans_isterm {G u A} (H : Stt.isterm G u A) {struct H} :
+  Ttt.isterm (trans_ctx G) (trans_term u) (trans_type A)
+
+with trans_issubst {sbs G D} (H : Stt.issubst sbs G D) {struct H} :
+  Ttt.issubst (trans_subst sbs) (trans_ctx G) (trans_ctx D)
+
+with trans_eqctx {G D} (H : Stt.eqctx G D) {struct H} :
+  Ttt.eqctx (trans_ctx G) (trans_ctx D)
+
+with trans_eqtype {G A B} (H : Stt.eqtype G A B) {struct H} :
+  Ttt.eqtype (trans_ctx G) (trans_type A) (trans_type B)
+
+with trans_eqterm {G u v A} (H : Stt.eqterm G u v A) {struct H} :
+  Ttt.eqterm (trans_ctx G) (trans_term u) (trans_term v) (trans_type A)
+
+with trans_eqsubst {sbs sbt G D} (H : Stt.eqsubst sbs sbt G D) {struct H} :
+  Ttt.eqsubst (trans_subst sbs) (trans_subst sbt) (trans_ctx G) (trans_ctx D).
+
+Proof.
+
+  (* trans_isctx *)
+  - { destruct H ; doConfig.
+
+      (* CtxEmpty *)
+      - constructor.
+
+      (* CtxExtend *)
+      - simpl. config constructor.
+        ih.
+    }
+
+  (* trans_istype *)
+  - { destruct H ; doConfig.
+
+      (* TyCtxConv *)
+      - { (config apply @TyCtxConv with (G := trans_ctx G)) ; ih. }
+
+      (* TySubst *)
+      - { simpl.
+          config apply @TySubst with (D := trans_ctx D).
+          - ih.
+          - ih.
+        }
+
+      (* TyProd *)
+      - { simpl.
+          capply TySimProd.
+          - todo. (* Where is the hypothesis that Ttt has simple products? *)
+          - capply TyProd.
+            now apply (trans_istype (ctxextend G A) B).
+          (* Too bad ih doesn't deal with this*)
+          - capply TyBool. ih.
+        }
+
+      (* TyId *)
+      - { simpl. capply TyId.
+          - ih.
+          - ih.
+        }
+
+      (* TyEmpty *)
+      - capply TyEmpty ; ih.
+
+      (* TyUnit *)
+      - capply TyUnit ; ih.
+
+      (* TyBool *)
+      - capply TyBool ; ih.
+
+      (* TySimProd *)
+      - { simpl. capply TySimProd.
+          - todo. (* I'm not good with type classes yet *)
+          - ih.
+          - ih.
+        }
+    }
+
+  (* trans_isterm *)
+  - todo.
+
+  (* trans_issubst *)
+  - todo.
+
+  (* trans_eqctx *)
+  - todo.
+
+  (* trans_eqtype *)
+  - todo.
+
+  (* trans_eqterm *)
+  - todo.
+
+  (* trans_eqsubst *)
+  - todo.
+Qed.
+
+End Translation.
