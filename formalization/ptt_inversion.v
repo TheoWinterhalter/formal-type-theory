86cc8b3bcc2d70a2f0bed08d36bb0b0a8121eb68
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-03-24 15:40:15 +0100

    An inversion lemma about ctxeq.

--- formalization/ptt_inversion.v
+++ formalization/ptt_inversion.v
@@ -11,6 +11,53 @@ Section PttInversion.
 Local Instance hasPrecond : config.Precond := {| config.precondFlag := config.Yes |}.
 Context `{ConfigReflection : config.Reflection}.
 
+Local Open Scope type_scope.
+
+Definition CtxEqInversion G1 G2 :
+  eqctx G1 G2 ->
+  ((G1 = ctxempty) * (G2 = ctxempty)) +
+  { D1 : context &
+  { A1 : type &
+  { D2 : context &
+  { A2 : type &
+    (G1 = ctxextend D1 A1) * (G2 = ctxextend D2 A2)
+  }}}}.
+Proof.
+  intro eq.
+  induction eq.
+
+  - { destruct G.
+      + now left.
+      + rename t into A ; right.
+        exists G, A, G, A ; auto.
+    }
+
+  - { destruct IHeq as [[Gempty Dempty] | H].
+
+     - now left.
+     - destruct H as (D1 & A1 & D2 & A2 & eq1 & eq2).
+       right.
+       now exists D2, A2, D1, A1.
+    }
+
+  - { destruct IHeq1 as [[Gempty Dempty] | [D1 [A1 [D2 [A2 [eq3 eq4]]]]]] ;
+      destruct IHeq2 as [[? ?] | [D3 [A3 [D4 [A4 [eq5 eq6]]]]]].
+
+      - now left.
+      - rewrite Dempty in eq5. discriminate eq5.
+      - rewrite e in eq4. discriminate eq4.
+      - right.
+        now exists D1, A1, D4, A4.
+    }
+
+  - now left.
+
+  - right.
+    now exists G, A, D, B.
+
+Defined.
+
+
 Definition CtxExtendInversion G A (H : isctx (ctxextend G A)) :
   isctx G * istype G A.
 Proof.
