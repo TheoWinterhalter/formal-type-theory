e27ecc4ad2955eec7c41cc5f3564961b764473cb
Théo Winterhalter <isheeft@gmail.com>
2017-04-18 17:47:09 +0200

    De Bruijn indices are a pain for the translation

--- src/forcing.v
+++ src/forcing.v
@@ -11,6 +11,8 @@ Require Import syntax.
 Require Import tt.
 Require Import checking_tactics.
 
+Require Import String.
+
 (* Source type theory *)
 Module Stt.
 
@@ -103,5 +105,24 @@ Definition comp p q r f g :=
 Context `{CompIdℙLeft : forall Γ p q f, Stt.isterm Γ f (Hom p q) -> Stt.eqterm Γ (comp p p q (idℙ p) f) f (Hom p q)}.
 Context `{CompIdℙRight : forall Γ p q f, Stt.isterm Γ f (Hom p q) -> Stt.eqterm Γ (comp p q q f (idℙ q)) f (Hom p q)}.
 Context `{CompℙAssoc : forall Γ p q r s f g h, Stt.isterm Γ f (Hom p q) -> Stt.isterm Γ g (Hom q r) -> Stt.isterm Γ h (Hom r s) -> Stt.eqterm Γ (comp p q s f (comp q r s g h)) (comp p r s (comp p q r f g) h) (Hom p s)}.
+
+(* We also introduce the notion of forcing context *)
+Inductive fctx :=
+| cond : string -> fctx
+| fxvar : fctx -> fctx
+| fxhom : fctx -> string -> string -> fctx.
+
+(* As well as a validity judgment for them *)
+Inductive isfctx : context -> fctx -> Type :=
+| valid_cond : forall p, isfctx ctxempty (cond p)
+| valid_fxhom : forall Γ σ q f, isfctx Γ σ -> isfctx Γ (fxhom σ q f)
+| valid_fxvar : forall Γ σ A, isfctx Γ σ -> isfctx (ctxextend Γ A) (fxvar σ).
+
+(* Notation "'λ' (q f : σ). M" := (lam (El ℙ) ? ?) *)
+
+(* Translation *)
+(* Fixpoint trans_ctx (σ : fctx) (Γ : context) : context := *)
+(*   match Γ with *)
+(*   | Uni l => lam (El ℙ) ? (lam (Hom ? (var 0))) *)
 
 End Translation.
