32c1452fb94e82ad3ccf583f01b7918c36ce8653
Théo Winterhalter <isheeft@gmail.com>
2017-05-31 15:33:52 +0200

    Partially fix config in tt

--- src/tt.v
+++ src/tt.v
@@ -6,23 +6,25 @@ Require Import config.
 Section TypeTheoryRules.
 (* Notations for writing down inference rules. *)
 
-Context `{ConfigPrecond : config.Precond}.
-Context `{ConfigReflection : config.Reflection}.
-Context `{ConfigSimpleProducts : config.SimpleProducts}.
-Context `{ConfigProdEta : config.ProdEta}.
-Context `{ConfigUniverses : config.Universes}.
-Context `{ConfigWithProp : config.WithProp}.
-Context `{ConfigWithJ : config.WithJ}.
-Context `{ConfigEmpty : config.WithEmpty}.
-Context `{ConfigUnit : config.WithUnit}.
-Context `{ConfigBool : config.WithBool}.
-Context `{ConfigPi : config.WithPi}.
-Context `{ConfigUniProd : config.UniProd}.
-Context `{ConfigUniId : config.UniId}.
-Context `{ConfigUniEmpty : config.UniEmpty}.
-Context `{ConfigUniUnit : config.UniUnit}.
-Context `{ConfigUniBool : config.UniBool}.
-Context `{ConfigUniSimProd : config.UniSimProd}.
+Context {ConfigSyntax : config.Syntax}.
+Context {ConfigPrecond : config.Precond}.
+Context {ConfigReflection : config.Reflection}.
+Context {ConfigSimpleProducts : config.SimpleProducts}.
+Context {ConfigProdEta : config.ProdEta}.
+Context {ConfigUniverseLevels : UniverseLevels}.
+Context {ConfigUniverses : config.Universes}.
+Context {ConfigWithProp : config.WithProp}.
+Context {ConfigWithJ : config.WithJ}.
+Context {ConfigEmpty : config.WithEmpty}.
+Context {ConfigUnit : config.WithUnit}.
+Context {ConfigBool : config.WithBool}.
+Context {ConfigPi : config.WithPi}.
+Context {ConfigUniProd : config.UniProd}.
+Context {ConfigUniId : config.UniId}.
+Context {ConfigUniEmpty : config.UniEmpty}.
+Context {ConfigUniUnit : config.UniUnit}.
+Context {ConfigUniBool : config.UniBool}.
+Context {ConfigUniSimProd : config.UniSimProd}.
 
 Notation "'rule' r 'endrule'" := (r) (at level 96, only parsing).
 
