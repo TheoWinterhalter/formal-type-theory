751991a78a47e7c9898db5b0b31fb42108c90ce5
Théo Winterhalter <isheeft@gmail.com>
2017-06-01 09:26:03 +0200

    Inversion is broken because we lost injectivity of constructors

--- src/ptt_inversion.v
+++ src/ptt_inversion.v
@@ -1,24 +1,31 @@
 (* Inversion theorems for ptt. *)
 
-Require config.
+Require Import config.
 Require Import config_tactics.
 
-Require Import syntax.
 Require Import tt.
 
 Section PttInversion.
 
 Local Instance hasPrecond : config.Precond := {| config.precondFlag := config.Yes |}.
-Context `{ConfigReflection : config.Reflection}.
-Context `{configSimpleProducts : config.SimpleProducts}.
-Context `{configProdEta : config.ProdEta}.
-Context `{ConfigUniverses : config.Universes}.
-Context `{ConfigWithProp : config.WithProp}.
-Context `{ConfigWithJ : config.WithJ}.
-Context `{ConfigEmpty : config.WithEmpty}.
-Context `{ConfigUnit : config.WithUnit}.
-Context `{ConfigBool : config.WithBool}.
-Context `{ConfigPi : config.WithPi}.
+Context {ConfigSyntax : config.Syntax}.
+Context {ConfigReflection : config.Reflection}.
+Context {ConfigSimpleProducts : config.SimpleProducts}.
+Context {ConfigProdEta : config.ProdEta}.
+Context {ConfigUniverseLevels : config.UniverseLevels}.
+Context {ConfigUniverses : config.Universes}.
+Context {ConfigWithProp : config.WithProp}.
+Context {ConfigWithJ : config.WithJ}.
+Context {ConfigEmpty : config.WithEmpty}.
+Context {ConfigUnit : config.WithUnit}.
+Context {ConfigBool : config.WithBool}.
+Context {ConfigPi : config.WithPi}.
+Context {ConfigUniProd : config.UniProd}.
+Context {ConfigUniId : config.UniId}.
+Context {ConfigUniEmpty : config.UniEmpty}.
+Context {ConfigUniUnit : config.UniUnit}.
+Context {ConfigUniBool : config.UniBool}.
+Context {ConfigUniSimProd : config.UniSimProd}.
 
 Definition CtxExtendInversion G A (H : isctx (ctxextend G A)) :
   isctx G * istype G A.
