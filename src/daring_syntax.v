2020f29e3d56a5d92e3f984bba06ef932f130a7c
Théo Winterhalter <isheeft@gmail.com>
2017-06-05 20:10:01 +0200

    Lifting and substitutions from Siles

--- src/daring_syntax.v
+++ src/daring_syntax.v
@@ -15,6 +15,10 @@
 
 Require config.
 
+Require Import Peano_dec.
+Require Import Compare_dec.
+Require Import Lt Le Gt.
+
 (* Universe levels *)
 Inductive level : Type :=
 | uni : nat -> level
@@ -36,9 +40,9 @@ with term : Type :=
      | Uni : level -> term
      (* Terms *)
      | var : nat -> term
-     | lam : term -> term
+     | lam : term -> term -> term
      | app : term -> term -> term
-     | refl : term -> term -> term
+     | refl : term -> term
      | j : term -> term -> term -> term -> term -> term -> term
      | exfalso : term -> term -> term
      | unit : term
@@ -49,6 +53,89 @@ with term : Type :=
      | proj1 : term -> term -> term -> term
      | proj2 : term -> term -> term -> term
 .
+
+(* Substitutions are taken from Siles et al.  *)
+Reserved Notation " t ↑ x # n " (at level 5, x at level 0, left associativity).
+
+Fixpoint lift_rec (n : nat) (k : nat) (t : term) {struct t}
+  := match t with
+     | Prod A B => Prod (A ↑ n # k) (B ↑ n # (S k))
+     | Id A u v => Id (A ↑ n # k) (u ↑ n # k) (v ↑ n # k)
+     | Empty => Empty
+     | Unit => Unit
+     | Bool => Bool
+     | SimProd A B => SimProd (A ↑ n # k) (B ↑ n # k)
+     | Uni l => Uni l
+
+     | var x => if le_gt_dec k x then var (n + x) else var x
+     | lam A t => lam (A ↑ n # k) (t ↑ n # (S k))
+     | app u v => app (u ↑ n # k) (v ↑ n # k)
+     | refl u => refl (u ↑ n # k)
+     | j A u C w v p =>
+       j (A ↑ n # k) (u ↑ n # k) (C ↑ n # (S (S k)))
+         (w ↑ n # k) (v ↑ n # k) (p ↑ n # k)
+     | exfalso A u => exfalso (A ↑ n # k) (u ↑ n # k)
+     | unit => unit
+     | true => true
+     | false => false
+     | cond C u v w =>
+       cond (C ↑ n # (S k)) (u ↑ n # k) (v ↑ n # k) (w ↑ n # k)
+     | pair A B u v =>
+       pair (A ↑ n # k) (B ↑ n # k) (u ↑ n # k) (v ↑ n # k)
+     | proj1 A B p =>
+       proj1 (A ↑ n # k) (B ↑ n # k) (p ↑ n # k)
+     | proj2 A B p =>
+       proj2 (A ↑ n # k) (B ↑ n # k) (p ↑ n # k)
+     end
+where "t ↑ n # k" := (lift_rec n k t).
+
+Notation " t ↑ n " := (lift_rec n 0 t)
+                        (at level 5, n at level 0, left associativity).
+Notation " t ↑ " := (lift_rec 1 0 t) (at level 5, left associativity).
+
+Reserved Notation "t [ x ← u ]" (at level 5, x at level 0, left associativity).
+
+Fixpoint subst_rec u t n {struct t} :=
+  match t with
+  | Prod A B => Prod (A [n ← u]) (B [S n ← u])
+  | Id A u v => Id (A [n ← u]) (u [n ← u]) (v [n ← u])
+  | Empty => Empty
+  | Unit => Unit
+  | Bool => Bool
+  | SimProd A B => SimProd (A [n ← u]) (B [n ← u])
+  | Uni l => Uni l
+
+  | var x => match (lt_eq_lt_dec x n) with
+            | inleft (left _) => var x (* x < n *)
+            | inleft (right _) => u ↑ n  (* x = n *)
+            | inright _ => var (x - 1) (* x > n *)
+            end
+  | lam A t => lam (A [n ← u]) (t [S n ← u])
+  | app a b => app (a [n ← u]) (b [n ← u])
+  | refl t => refl (t [n ← u])
+  | j A a C w v p =>
+    j (A [n ← u]) (a [n ← u]) (C [S (S n) ← u])
+      (w [n ← u]) (v [n ← u]) (p [n ← u])
+  | exfalso A a => exfalso (A [n ← u]) (a [n ← u])
+  | unit => unit
+  | true => true
+  | false => false
+  | cond C a b c => cond (C [S n ← u]) (a [n ← u]) (b [n ← u]) (c [n ← u])
+  | pair A B a b =>
+    pair (A [n ← u]) (B [n ← u]) (a [n ← u]) (b [n ← u])
+  | proj1 A B p =>
+    proj1 (A [n ← u]) (B [n ← u]) (p [n ← u])
+  | proj2 A B p =>
+    proj2 (A [n ← u]) (B [n ← u]) (p [n ← u])
+  end
+where " t [ n ← u ] " := (subst_rec u t n).
+
+Notation " t [ ← u ] " := (subst_rec u t 0) (at level 5).
+
+(* We still need to define a type for substitutions and composition. *)
+(* We could always choose substitution := term -> term
+   and subst u sbs := sbs u *)
+
 
 Definition exactly : forall {F A}, A -> config.Flag F -> A.
 Proof.
