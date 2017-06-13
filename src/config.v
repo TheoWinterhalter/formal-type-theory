b64e4eb0493382f49b3fa630fddb249599d4f84b
Théo Winterhalter <isheeft@gmail.com>
2017-06-13 14:34:00 +0200

    Just trying to rely more on type classes, but…

--- src/config.v
+++ src/config.v
@@ -94,52 +94,52 @@ Class Syntax `{Universes, WithProp, WithPi, WithEmpty, WithUnit, WithBool,
   level        : Type ;
 
   (* Universe levels *)
-  uni  : forall {_ : Flag universesFlag}, nat -> level ;
-  prop : forall {_ : Flag withpropFlag}, level ;
+  uni  : forall `{_ : Flag universesFlag}, nat -> level ;
+  prop : forall `{_ : Flag withpropFlag}, level ;
 
   (* Context constructors *)
   ctxempty  : context ;
   ctxextend : context -> type -> context ;
 
   (* Types *)
-  Prod    : forall {_ : Flag withpiFlag}, type -> type -> type ;
+  Prod    : forall `{_ : Flag withpiFlag}, type -> type -> type ;
   Id      : type -> term -> term -> type ;
   Subst   : type -> substitution -> type ;
-  Empty   : forall {_ : Flag withemptyFlag}, type ;
-  Unit    : forall {_ : Flag withunitFlag}, type ;
-  Bool    : forall {_ : Flag withboolFlag}, type ;
-  SimProd : forall {_ : Flag simpleproductsFlag}, type -> type -> type ;
-  Uni     : forall {_ : Flag universesFlag}, level -> type ;
-  El      : forall {_ : Flag universesFlag}, level -> term -> type ;
+  Empty   : forall `{_ : Flag withemptyFlag}, type ;
+  Unit    : forall `{_ : Flag withunitFlag}, type ;
+  Bool    : forall `{_ : Flag withboolFlag}, type ;
+  SimProd : forall `{_ : Flag simpleproductsFlag}, type -> type -> type ;
+  Uni     : forall `{_ : Flag universesFlag}, level -> type ;
+  El      : forall `{_ : Flag universesFlag}, level -> term -> type ;
 
   (* Terms *)
   var : nat -> term ;
-  lam : forall {_ : Flag withpiFlag}, type -> type -> term -> term ;
-  app : forall {_ : Flag withpiFlag}, term -> type -> type -> term -> term ;
+  lam : forall `{_ : Flag withpiFlag}, type -> type -> term -> term ;
+  app : forall `{_ : Flag withpiFlag}, term -> type -> type -> term -> term ;
   refl : type -> term -> term ;
-  j : forall {_ : Flag withjFlag}, type -> term -> type -> term -> term -> term -> term ;
+  j : forall `{_ : Flag withjFlag}, type -> term -> type -> term -> term -> term -> term ;
   subst : term -> substitution -> term ;
-  pair : forall {_ : Flag simpleproductsFlag}, type -> type -> term -> term -> term ;
-  proj1 : forall {_ : Flag simpleproductsFlag}, type -> type -> term -> term ;
-  proj2 : forall {_ : Flag simpleproductsFlag}, type -> type -> term -> term ;
-  exfalso : forall {_ : Flag withemptyFlag}, type -> term -> term ;
-  unit : forall {_ : Flag withunitFlag}, term ;
-  true : forall {_ : Flag withboolFlag}, term ;
-  false : forall {_ : Flag withboolFlag}, term ;
-  cond : forall {_ : Flag withboolFlag}, type -> term -> term -> term -> term ;
-  uniProd : forall {_ : Flag universesFlag}, forall {_ : Flag withpiFlag},
+  pair : forall `{_ : Flag simpleproductsFlag}, type -> type -> term -> term -> term ;
+  proj1 : forall `{_ : Flag simpleproductsFlag}, type -> type -> term -> term ;
+  proj2 : forall `{_ : Flag simpleproductsFlag}, type -> type -> term -> term ;
+  exfalso : forall `{_ : Flag withemptyFlag}, type -> term -> term ;
+  unit : forall `{_ : Flag withunitFlag}, term ;
+  true : forall `{_ : Flag withboolFlag}, term ;
+  false : forall `{_ : Flag withboolFlag}, term ;
+  cond : forall `{_ : Flag withboolFlag}, type -> term -> term -> term -> term ;
+  uniProd : forall `{_ : Flag universesFlag}, forall `{_ : Flag withpiFlag},
             level -> level -> term -> term -> term ;
-  uniId : forall {_ : Flag universesFlag}, (* forall {_ : Flag withidFlag}, *)
+  uniId : forall `{_ : Flag universesFlag}, (* forall `{_ : Flag withidFlag}, *)
           level -> term -> term -> term -> term ;
-  uniEmpty : forall {_ : Flag universesFlag}, forall {_ : Flag withemptyFlag},
+  uniEmpty : forall `{_ : Flag universesFlag}, forall `{_ : Flag withemptyFlag},
              level -> term ;
-  uniUnit : forall {_ : Flag universesFlag}, forall {_ : Flag withunitFlag},
+  uniUnit : forall `{_ : Flag universesFlag}, forall `{_ : Flag withunitFlag},
              level -> term ;
-  uniBool : forall {_ : Flag universesFlag}, forall {_ : Flag withboolFlag},
+  uniBool : forall `{_ : Flag universesFlag}, forall `{_ : Flag withboolFlag},
              nat -> term ;
-  uniSimProd : forall {_ : Flag universesFlag}, forall {_ : Flag simpleproductsFlag},
+  uniSimProd : forall `{_ : Flag universesFlag}, forall `{_ : Flag simpleproductsFlag},
                level -> level -> term -> term -> term ;
-  uniUni : forall {_ : Flag universesFlag}, level -> term ;
+  uniUni : forall `{_ : Flag universesFlag}, level -> term ;
 
   (* Explicit substitutions *)
   sbzero  : type -> term -> substitution ;
