4ba2c85fd7ef3b871a538cce84b06dc83a3cfcb8
Théo Winterhalter <isheeft@gmail.com>
2017-11-06 16:57:39 +0100

    Add paranoid syntax and the corresponding inversion principle

---
+++ src/paranoid_syntax.v
@@ -0,0 +1,136 @@
+(* Paranoid Syntax
+
+   We propose an implementation of syntax that corresponds to the syntax with
+   which this library was designed at first.
+   It features all the specifics of the language as mutual inductive types.
+
+   This includes explicit substitutions, seperate syntaxes for types and terms,
+   optional Tarski universes and so on...
+
+   This syntax is still configurable. For instance, if you decide not to have
+   the Unit type as part of your theory, it will still be part of the syntax,
+   but you won't be able to derive that [Γ ⊢ Unit] type or [Γ ⊢ unit : Unit].
+*)
+
+Require config syntax tt.
+
+Section ParanoidSyntax.
+
+Context `{configPrecond : config.Precond}.
+Context `{configReflection : config.Reflection}.
+Context `{configSimpleProducts : config.SimpleProducts}.
+Context `{configProdEta : config.ProdEta}.
+Context `{ConfigUniverses : config.Universes}.
+Context `{ConfigWithProp : config.WithProp}.
+Context `{ConfigId : config.IdentityTypes}.
+Context `{ConfigWithJ : config.WithJ}.
+Context `{ConfigEmpty : config.WithEmpty}.
+Context `{ConfigUnit : config.WithUnit}.
+Context `{ConfigBool : config.WithBool}.
+Context `{ConfigPi : config.WithPi}.
+
+Inductive context : Type :=
+     | ctxempty : context
+     | ctxextend : context -> type -> context
+
+with type : Type :=
+     | Prod : type -> type -> type
+     | Id : type -> term -> term -> type
+     | Subst : type -> substitution -> type
+     | Empty : type
+     | Unit : type
+     | Bool : type
+     | SimProd : type -> type -> type
+     | Uni : syntax.level -> type
+     | El : syntax.level -> term -> type
+
+with term : Type :=
+     | var : nat -> term
+     | lam : type -> type -> term -> term
+     | app : term -> type -> type -> term -> term
+     | refl : type -> term -> term
+     | j : type -> term -> type -> term -> term -> term -> term
+     | subst : term -> substitution -> term
+     | exfalso : type -> term -> term
+     | unit : term
+     | true : term
+     | false : term
+     | cond : type -> term -> term -> term -> term
+     | pair : type -> type -> term -> term -> term
+     | proj1 : type -> type -> term -> term
+     | proj2 : type -> type -> term -> term
+     | uniProd : syntax.level -> syntax.level -> term -> term -> term
+     | uniId : syntax.level -> term -> term -> term -> term
+     | uniEmpty : syntax.level -> term
+     | uniUnit : syntax.level -> term
+     | uniBool : nat -> term
+     | uniSimProd : syntax.level -> syntax.level -> term -> term -> term
+     | uniUni : syntax.level -> term
+
+with substitution : Type :=
+     | sbzero : type -> term -> substitution
+     | sbweak : type -> substitution
+     | sbshift : type -> substitution -> substitution
+     | sbid : substitution
+     | sbcomp : substitution -> substitution -> substitution
+.
+
+Local Instance Syntax : syntax.Syntax := {|
+  syntax.context      := context ;
+  syntax.type         := type ;
+  syntax.term         := term ;
+  syntax.substitution := substitution ;
+
+  syntax.ctxempty  := ctxempty ;
+  syntax.ctxextend := ctxextend ;
+
+  syntax.Prod    := Prod ;
+  syntax.Id      := Id ;
+  syntax.Subst   := Subst ;
+  syntax.Empty   := Empty ;
+  syntax.Unit    := Unit ;
+  syntax.Bool    := Bool ;
+  syntax.SimProd := SimProd ;
+  syntax.Uni     := Uni ;
+  syntax.El      := El  ;
+
+  syntax.var        := var ;
+  syntax.lam        := lam ;
+  syntax.app        := app ;
+  syntax.refl       := refl ;
+  syntax.j          := j ;
+  syntax.subst      := subst ;
+  syntax.exfalso    := exfalso ;
+  syntax.unit       := unit ;
+  syntax.true       := true  ;
+  syntax.false      := false ;
+  syntax.cond       := cond ;
+  syntax.pair       := pair ;
+  syntax.proj1      := proj1 ;
+  syntax.proj2      := proj2 ;
+  syntax.uniProd    := uniProd ;
+  (* syntax.uniId      := uniId ; *)
+  syntax.uniId      := uniId ;
+  syntax.uniEmpty   := uniEmpty ;
+  syntax.uniUnit    := uniUnit ;
+  syntax.uniBool    := uniBool ;
+  syntax.uniSimProd := uniSimProd ;
+  syntax.uniUni     := uniUni ;
+
+  syntax.sbzero  := sbzero ;
+  syntax.sbweak  := sbweak ;
+  syntax.sbshift := sbshift ;
+  syntax.sbid    := sbid ;
+  syntax.sbcomp  := sbcomp
+|}.
+
+Definition isctx := tt.isctx.
+Definition issubst := tt.issubst.
+Definition istype := tt.istype.
+Definition isterm := tt.isterm.
+Definition eqctx := tt.eqctx.
+Definition eqsubst := tt.eqsubst.
+Definition eqtype := tt.eqtype.
+Definition eqterm := tt.eqterm.
+
+End ParanoidSyntax.
---
+++ src/ps_inversion.v
@@ -0,0 +1,124 @@
+(* Paranoid syntax inversion
+
+   The purpose of this file is to provide the inversion lemmata that are
+   required by sanity on paranoid syntax.
+   These lemmata are proven only in the case of paranoid rules.
+*)
+
+Require config syntax.
+Require invconfig.
+Require Import paranoid_syntax.
+Require Import config_tactics.
+
+Section ParanoidSyntaxInversion.
+
+Local Instance hasPrecond : config.Precond := {|
+  config.precondFlag := config.Yes
+|}.
+Context `{configReflection : config.Reflection}.
+Context `{configSimpleProducts : config.SimpleProducts}.
+Context `{configProdEta : config.ProdEta}.
+Context `{ConfigUniverses : config.Universes}.
+Context `{ConfigWithProp : config.WithProp}.
+Context `{ConfigId : config.IdentityTypes}.
+Context `{ConfigWithJ : config.WithJ}.
+Context `{ConfigEmpty : config.WithEmpty}.
+Context `{ConfigUnit : config.WithUnit}.
+Context `{ConfigBool : config.WithBool}.
+Context `{ConfigPi : config.WithPi}.
+
+Definition CtxExtendInversion G A (H : isctx (ctxextend G A)) :
+  isctx G * istype G A.
+Proof.
+  config inversion H. easy.
+Defined.
+
+Fixpoint TyIdInversion G A u v (H : istype G (Id A u v)) {struct H} :
+  isctx G * istype G A * isterm G u A * isterm G v A.
+Proof.
+  inversion H ; doConfig.
+
+  - { split ; [(split ; [split | idtac]) | idtac].
+
+      - assumption.
+      - apply @tt.TyCtxConv with (G := G0) ; auto.
+        now apply TyIdInversion with (u := u) (v := v).
+      - apply @tt.TermCtxConv with (G := G0) ; auto.
+        + now apply TyIdInversion with (u := u) (v:= v).
+        + now config apply TyIdInversion with (u := u) (v:= v).
+      - apply @tt.TermCtxConv with (G := G0) ; auto.
+        + now apply TyIdInversion with (u := u) (v:= v).
+        + now config apply TyIdInversion with (u := u) (v:= v).
+    }
+
+  - { split ; [(split ; [split | idtac]) | idtac].
+      - assumption.
+      - assumption.
+      - assumption.
+      - assumption.
+    }
+
+Defined.
+
+Fixpoint TyProdInversion G A B (H : istype G (Prod A B)) {struct H} :
+  isctx G * istype G A * istype (ctxextend G A) B.
+Proof.
+  inversion H ; doConfig.
+
+  - { split ; [ split | idtac ].
+      - assumption.
+      - apply @tt.TyCtxConv with (G := G0) ; auto.
+        now apply (TyProdInversion G0 A B).
+      - apply @tt.TyCtxConv with (G := ctxextend G0 A).
+        + now apply (TyProdInversion G0 A B).
+        + apply @tt.EqCtxExtend ; auto.
+          * now capply (TyProdInversion G0 A B).
+          * now capply (TyProdInversion G0 A B).
+          * capply @tt.EqTyRefl ; auto.
+            now apply (TyProdInversion G0 A B).
+        + capply @tt.CtxExtend ; auto.
+          now apply (TyProdInversion G0 A B).
+        + capply @tt.CtxExtend.
+          * assumption.
+          * apply @tt.TyCtxConv with (G := G0) ; auto.
+            now apply (TyProdInversion G0 A B).
+    }
+
+  - { split ; [ split | idtac ].
+      - assumption.
+      - assumption.
+      - assumption.
+    }
+Defined.
+
+Fixpoint TySimProdInversion G A B (H : istype G (SimProd A B)) {struct H} :
+  isctx G * istype G A * istype G B.
+Proof.
+  inversion H ; doConfig.
+
+  - { split ; [ split | .. ].
+      - assumption.
+      - apply @tt.TyCtxConv with (G := G0) ; auto.
+        now apply (TySimProdInversion G0 A B).
+      - apply @tt.TyCtxConv with (G := G0) ; auto.
+        now apply (TySimProdInversion G0 A B).
+    }
+
+  - { split ; [ split | .. ] ; assumption. }
+Defined.
+
+Local Instance LocSyntax : syntax.Syntax := Syntax.
+
+Local Instance CtxExtendInversionInstance : invconfig.CtxExtendInversionClass
+  := {| invconfig.CtxExtendInversion := CtxExtendInversion |}.
+
+Local Instance TyIdInversionInstance : invconfig.TyIdInversionClass
+  := {| invconfig.TyIdInversion := TyIdInversion |}.
+
+Local Instance TyProdInversionInstance : invconfig.TyProdInversionClass
+  := {| invconfig.TyProdInversion := TyProdInversion |}.
+
+Local Instance TySimProdInversionInstance : invconfig.TySimProdInversionClass
+  := {| invconfig.TySimProdInversion := TySimProdInversion |}.
+
+End ParanoidSyntaxInversion.
