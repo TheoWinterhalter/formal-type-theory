71d51af7b7192a4a513edaad3e8601498c205acc
Théo Winterhalter <isheeft@gmail.com>
2017-12-15 12:51:14 +0000

    Switching to `nat -> term` representation of substitutions
    Add definitions of the previous substitutions. May require changes.

--- src/syntax.v
+++ src/syntax.v
@@ -1,3 +1,4 @@
+Require Import Coq.Lists.List.
 Open Scope list_scope.
 
 (* Universe levels *)
@@ -7,13 +8,12 @@ Inductive level : Type :=
 .
 
 Class Syntax := {
-  (* context : Type; *)
+  context : Type;
   type : Type;
   term : Type;
   (* substitution : Type; *)
 
-  context := list type;
-  substitution := list term;
+  substitution := nat -> term;
 
   (* ctxempty : context; *)
   (* ctxextend : context -> type -> context; *)
@@ -50,17 +50,36 @@ Class Syntax := {
   uniBinaryProd : level -> level -> term -> term -> term;
   uniUni : level -> term;
 
-  (* How do you define weakening?
-     We probably should have context be inductive, for instance by having
-     context := list type
-     and then maybe also
-     substitution := list (type * term).
-   *)
-  sbweak := fix w (Γ : context) {struct Γ} : substitution :=
-    match Γ with
-    | nil => nil
-    | B :: Δ => var 1 :: w Δ
-    end;
+  sbzero : term -> substitution :=
+    fun (u : term) (n : nat) =>
+      match n with
+      | 0 => u
+      | S n => var n
+      end;
+
+  sbweak : substitution :=
+    fun n => var (S n);
+
+  sbshift : substitution -> substitution :=
+    fun (σ : substitution) (n : nat) =>
+      match n with
+      | 0 => var 0
+      | S n => subst (σ n) sbweak
+      end;
+
+  sbid : substitution :=
+    fun n => var n;
+
+  sblist : list term -> substitution :=
+    fix rec (l : list term) (n : nat) :=
+      match l with
+      | nil => var n
+      | u :: σl =>
+        match n with
+        | 0 => u
+        | S n => rec (map (fun x => subst x sbweak) σl) n
+        end
+      end;
 
   (* sbzero : type -> term -> substitution; *)
   (* sbweak : type -> substitution; *)
