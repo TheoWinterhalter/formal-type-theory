22ef2a87e33b52c9a3fb484fb87a5b10edc9597e
Théo Winterhalter <isheeft@gmail.com>
2017-12-18 11:19:18 +0100

    Add tactics to rewrite substitutions

--- src/checking_tactics.v
+++ src/checking_tactics.v
@@ -1,9 +1,64 @@
-(* Tactics and auxiliary lemmas for ptt. *)
+(* Tactics and auxiliary lemmas for deriving judgments. *)
 
 Require config.
 Require Import config_tactics.
 Require Import tt.
 Require Import syntax.
+
+(* Tactics to apply rules of substitutions *)
+Ltac rewrite_Subst_action :=
+  first [
+    rewrite SubstProd
+  | rewrite SubstId
+  | rewrite SubstEmpty
+  | rewrite SubstUnit
+  | rewrite SubstBool
+  | rewrite SubstBinaryProd
+  | rewrite SubstUni
+  | rewrite SubstEl
+  | rewrite sbidtype
+  ].
+
+Ltac rewrite_subst_action :=
+  first [
+    rewrite substLam
+  | rewrite substApp
+  | rewrite substRefl
+  | rewrite substJ
+  | rewrite substExfalso
+  | rewrite substUnit
+  | rewrite substTrue
+  | rewrite substFalse
+  | rewrite substCond
+  | rewrite substPair
+  | rewrite substProjOne
+  | rewrite substProjTwo
+  | rewrite substUniProd
+  | rewrite substUniId
+  | rewrite substUniEmpty
+  | rewrite substUniUnit
+  | rewrite substUniBool
+  | rewrite substUniBinaryProd
+  | rewrite substUniUni
+  | rewrite sbidterm
+  ].
+
+Ltac rewrite_subst_var :=
+  first [
+    rewrite sbconszero
+  | rewrite sbconssucc
+  | rewrite sbweakvar
+  ].
+
+Ltac rewrite_subst :=
+  first [
+    rewrite_Subst_action
+  | rewrite_subst_action
+  | rewrite_subst_var
+  ].
+
+
+(*! OLD BELOW !*)
 
 (* Configuration options for the tactics. *)
 Inductive magic_try := DoTry | DontTry.
