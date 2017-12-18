52ba8f46a5c5101fddc3efdc1fbeccbda5473286
Théo Winterhalter <isheeft@gmail.com>
2017-12-18 17:23:43 +0100

    First attempt of checking tactic
    It doesn't handle substitutions yet if they do not compute. We would indeed need to have some TySubst and TermSubst to do so.

--- src/checking_tactics.v
+++ src/checking_tactics.v
@@ -300,12 +300,65 @@ Ltac check_step_factory debug shelf apsym apptac ktac :=
     | capply CtxSym ; [ assumption | .. ]
     | myfail debug
     ]
-    else tryif (is_evar Γ + is_evar Δ)
+    else tryif (is_evar Γ || is_evar Δ)
       then myshelve debug shelf
       else first [
         eqctxintrorule apptac
       | apptac CtxSym ; [ eqctxintrorule apptac | .. ]
       | apptac CtxRefl
+      | myfail debug
+      ] ; ktac debug shelf DoSym apptac
+
+  (* Equality of types *)
+  | |- eqtype ?Γ ?A ?B =>
+    tryif (is_var A ; is_var B)
+    then first [
+      eassumption
+    | ceapply EqTyRefl
+    | ceapply EqTySym ; [ eassumption | .. ]
+    | ceapply EqTyCtxConv ; [
+            first [
+              eassumption
+            | ceapply EqTySym ; [ eassumption | .. ]
+            ]
+          | ..
+          ]
+    | myfail debug
+    ] ; ktac debug shelf DoSym apptac
+    else tryif (is_evar A || is_evar B)
+      then myshelve debug shelf
+      else first [
+        eqtypeintrorule apptac
+      | apptac EqTySym ; [ eqtypeintrorule apptac | .. ]
+      | apptac EqTyRefl
+      | myfail debug
+      ] ; ktac debug shelf DoSym apptac
+
+  (* Equality of terms *)
+  | |- eqterm ?Γ ?u ?v ?A =>
+    tryif (is_var u ; is_var v)
+    then first [
+      eassumption
+    | ceapply EqRefl
+    | ceapply EqSym ; [ eassumption |.. ]
+    | ceapply EqTyConv ; [ eassumption | .. ]
+    | ceapply EqTyConv ; [
+        ceapply EqSym ; [ eassumption | .. ]
+      | ..
+      ]
+    | myfail debug
+    ] ; ktac debug shelf DoSym apptac
+    else tryif (is_evar u || is_evar v)
+      then myshelve debug shelf
+      else first [
+        eqtermintrorule
+      | apptac EqSym ; [ eqtermintrorule apptac | .. ]
+      | ceapply EqTyConv ; [ eqtermintrorule apptac | .. ]
+      | ceapply EqTyConv ; [
+          apptac EqSym ; [ eqtermintrorule apptac | .. ]
+        | ..
+        ]
+      | apptac EqRefl
       | myfail debug
       ] ; ktac debug shelf DoSym apptac
 
@@ -335,287 +388,3 @@ Ltac echeckstep := check_step DoDebug DoShelf DoSym app_ceapply.
 
 Ltac check := check_f DoDebug DoShelf DoSym app_capply.
 Ltac echeck := check_f DoDebug DoShelf DoSym app_ceapply.
-
-(*! OLD BELOW !*)
-
-Inductive magic_try := DoTry | DontTry.
-Inductive magic_dotysym := DoTysym | DontTysym.
-Inductive magic_doeqsym := DoEqsym | DontEqsym.
-
-Ltac do_try flag :=
-  match flag with
-  | DoTry => idtac
-  | DontTry => fail "Cannot try"
-  end.
-
-Ltac do_tysym flag :=
-  match flag with
-  | DoTysym => idtac
-  | DontTysym => fail "Cannot do TySym"
-  end.
-
-Ltac do_eqsym flag :=
-  match flag with
-  | DoEqsym => idtac
-  | DontEqsym => fail "Cannot do EqSym"
-  end.
-
-(* Checking if we're dealing with a suitable goal. *)
-(* This would be interesting in another file maybe? *)
-Ltac check_goal :=
-  doConfig ;
-  lazymatch goal with
-  | |- isctx ?G => idtac
-  | |- istype ?G ?A => idtac
-  | |- isterm ?G ?u ?A => idtac
-  | |- eqctx ?G ?D => idtac
-  | |- eqtype ?G ?A ?B => idtac
-  | |- eqterm ?G ?u ?v ?A => idtac
-  | |- ?G => fail "Goal" G " is not a goal meant to be handled by magic."
-  end.
-
-(* Magic Tactic *)
-(* It is basically a type checker that doesn't do the smart things,
-   namely type and context conversions (and it doesn't rely on reflection
-   obviously). *)
-Ltac magicn try shelf tysym debug :=
-  doConfig ;
-  (* We only ever apply magic to suitable goals *)
-  check_goal ;
-  first [
-    assumption (* Why can't I remove it? *)
-  | (* We have several things we need to do to the tactic:
-       * Remove the _ case.
-       * Add a token to solve equalities with only one side as reflexivity.
-         (Maybe shelve them in the meantime?)
-       * We can take advantage of the information we have at hand on
-         substitutions to make magic finer!
-     *)
-    lazymatch goal with
-    | |- isterm ?G (var ?k) ?A =>
-      (* In that case, we might shelve, if the don't know the context. *)
-      tryif (is_evar G)
-      then shelve
-      else first [
-        eassumption
-      | myfail debug
-      ]
-    | [ H : isterm ?G ?v ?A, H' : isterm ?G ?v ?B |- isterm ?G ?v ?C ] =>
-      (* We have several options so we don't take any risk. *)
-      (* Eventually this should go away. I don't want to do the assert thing
-         anymore. *)
-      first [
-        is_var A ; exact H
-      | is_var B ; exact H'
-      | do_shelf shelf ; shelve
-      ]
-
-    (*! Equality of contexts !*)
-    | |- eqctx ?G ?G =>
-      first [
-        ceapply CtxRefl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-
-    (*! Equality of types !*)
-    | |- eqtype ?G (Id ?A ?u ?v) (Id ?B ?w ?z) =>
-      first [
-        ceapply CongId
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G (Prod ?A ?B) (Prod ?C ?D) =>
-      first [
-        ceapply CongProd
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G Unit Unit =>
-      first [
-        ceapply EqTyRefl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G Bool Bool =>
-      first [
-        ceapply EqTyRefl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G (BinaryProd _ _) (BinaryProd _ _) =>
-      first [
-        ceapply CongBinaryProd
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G (Uni _) (Uni _) =>
-      first [
-        ceapply EqTyRefl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G (El ?l ?a) (El ?l' ?b) =>
-      first [
-        ceapply CongEl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqtype ?G ?A ?B =>
-      tryif (is_var A ; is_var B)
-      then (
-        first [
-          eassumption
-        | ceapply EqTyRefl
-        | ceapply EqTySym ; [ eassumption | .. ]
-        | ceapply EqTyCtxConv ; [
-            first [
-              eassumption
-            | ceapply EqTySym ; [ eassumption | .. ]
-            ]
-          | ..
-          ]
-        | myfail debug
-        ] ; magicn try shelf DoTysym debug
-      )
-      else tryif (is_evar A || is_evar B)
-        then tryif (do_shelf shelf)
-          then shelve
-          else myfail debug
-        else myfail debug
-
-    (*! Equality of terms !*)
-    | |- eqterm ?G ?u ?u ?A =>
-      first [
-        ceapply EqRefl
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G ?u ?v Empty =>
-      first [
-        config eapply @EqTermExfalso with (w := u)
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G ?u ?v Unit =>
-      first [
-        ceapply UnitEta
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    (* Where should ProdBeta be handled? *)
-    (* Same for CondTrue, CondFalse, JRefl *)
-    (* ProdEta should come in after CongApp and CongProd probably *)
-    | |- eqterm ?G (lam ?A1 ?A2 ?u1) (lam ?B1 ?B2 ?u2) _ =>
-      first [
-        ceapply CongAbs
-      | ceapply EqTyConv ; [ ceapply CongAbs | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (app ?u1 ?A1 ?A2 ?u2) (app ?v1 ?B1 ?B2 ?v2) _ =>
-      first [
-        ceapply CongApp
-      | ceapply EqTyConv ; [ ceapply CongApp | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (refl ?A1 ?u1) (refl ?A2 ?u2) _ =>
-      first [
-        ceapply CongRefl
-      | ceapply EqTyConv ; [ ceapply CongRefl | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (j ?A1 ?u1 ?C1 ?w1 ?v1 ?p1) (j ?A2 ?u2 ?C2 ?w2 ?v2 ?p2) _ =>
-      first [
-        ceapply CongJ
-      | ceapply EqTyConv ; [ ceapply CongJ | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (cond ?C1 ?u1 ?v1 ?w1) (cond ?C2 ?u2 ?v2 ?w2) _ =>
-      first [
-        ceapply CongCond
-      | ceapply EqTyConv ; [ ceapply CongCond | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (pair _ _ _ _) (pair _ _ _ _) _ =>
-      first [
-        ceapply CongPair
-      | ceapply EqTyConv ; [ ceapply CongPair | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (proj1 _ _ _ ) (proj1 _ _ _) _ =>
-      first [
-        ceapply CongProjOne
-      | ceapply EqTyConv ; [ ceapply CongProjOne | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (proj2 _ _ _ ) (proj2 _ _ _) _ =>
-      first [
-        ceapply CongProjTwo
-      | ceapply EqTyConv ; [ ceapply CongProjTwo | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (uniProd _ prop _ _) (uniProd _ prop _ _) _ =>
-      first [
-        ceapply CongUniProdProp
-      | ceapply EqTyConv ; [ ceapply CongUniProdProp | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (uniProd _ _ _ _) (uniProd _ _ _ _) _ =>
-      first [
-        ceapply CongUniProd
-      | ceapply EqTyConv ; [ ceapply CongUniProd | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (uniId _ _ _ _) (uniId _ _ _ _) _ =>
-      first [
-        ceapply CongUniId
-      | ceapply EqTyConv ; [ ceapply CongUniId | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (uniBinaryProd prop prop _ _) (uniBinaryProd prop prop _ _) _ =>
-      first [
-        ceapply CongUniBinaryProdProp
-      | ceapply EqTyConv ; [ ceapply CongUniBinaryProdProp | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G (uniBinaryProd _ _ _ _) (uniBinaryProd _ _ _ _) _ =>
-      first [
-        ceapply CongUniBinaryProd
-      | ceapply EqTyConv ; [ ceapply CongUniBinaryProd | .. ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-    | |- eqterm ?G ?u ?v ?A =>
-      tryif (is_var u ; is_var v)
-      then first [
-        eassumption
-      | ceapply EqRefl
-      | ceapply EqSym ; [ eassumption |.. ]
-      | ceapply EqTyConv ; [ eassumption | .. ]
-      | ceapply EqTyConv ; [
-          ceapply EqSym ; [ eassumption | .. ]
-        | ..
-        ]
-      | myfail debug
-      ] ; magicn try shelf DoTysym debug
-      else tryif (is_evar u + is_evar v)
-        then tryif (do_shelf shelf)
-          then shelve
-          else myfail debug
-        else myfail debug
-
-    | _ => myfail debug
-    end
-  | do_try try
-  ].
-
-Ltac magic       := preop ; magicn DontTry DoShelf   DoTysym DoDebug.
-Ltac okmagic     := preop ; magicn DontTry DoShelf   DoTysym DontDebug.
-Ltac trymagic    := preop ; magicn DoTry   DoShelf   DoTysym DontDebug.
-Ltac strictmagic := preop ; magicn DontTry DontShelf DoTysym DoDebug.
-
-(* Tactic to keep judgments *)
-Ltac keep_ju :=
-  doConfig ;
-  first [
-    check_goal
-  | shelve
-  ].
-
-(* Tactic to keep equalities *)
-Ltac keep_eq :=
-  doConfig ;
-  lazymatch goal with
-  | |- eqterm _ _ _ _ => idtac
-  | |- eqtype _ _ _ => idtac
-  | |- eqctx _ _ => idtac
-  | _ => shelve
-  end.
