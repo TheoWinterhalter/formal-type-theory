43d243e4619c03dba53cc14bd88b16eb75bc4bf6
Théo Winterhalter <isheeft@gmail.com>
2017-12-20 12:24:21 +0100

    New definition of issubst, still hard to prove substitution lemma

--- src/substitutions.v
+++ src/substitutions.v
@@ -77,30 +77,70 @@ Context `{configBoolType : config.BoolType}.
 Context `{configProdType : config.ProdType}.
 Context `{configSyntax : syntax.Syntax}.
 
-(* First we need to define the notion of well-formed substitution. *)
-Inductive issubst : substitution -> context -> context -> Type :=
-| SubstEmpty :
-    forall {σ Γ},
-      isctx Γ ->
-      issubst σ Γ ctxempty
+(* A substitution is well-formed if it is well-formed on variables.
+   The action on types and terms is derived from it.
+ *)
+Definition issubst (σ : substitution) (Γ Δ : context) : Type :=
+  forall n A, isterm Δ (var n) A -> isterm Γ (var n)[← σ] A[σ].
 
-| SubstCons :
-    forall {σ u Γ Δ A},
-      istype Δ A ->
-      issubst σ Γ Δ ->
-      isterm Γ u A[σ] ->
-      issubst (u ⋅ σ) Γ (ctxextend Δ A)
+Lemma SubstId :
+  forall {Γ},
+    isctx Γ ->
+    issubst sbid Γ Γ.
+Proof.
+  intros Γ hΓ n A h.
+  rewrite_substs. assumption.
+Defined.
 
-| SubstId :
-    forall {Γ},
-      isctx Γ ->
-      issubst sbid Γ Γ
+Lemma SubstWeak :
+  forall {Γ A},
+    (config.flagPrecondition -> isctx Γ) ->
+    istype Γ A ->
+    issubst sbweak (ctxextend Γ A) Γ.
+Proof.
+  intros Γ A hΓ hA n B h.
+  rewrite_substs.
+  capply TermVarSucc.
+  - assumption.
+  - admit. (* Missing hyp *)
+  - assumption.
+Admitted.
 
-| SubstWeak :
-    forall {Γ A},
-      istype Γ A ->
-      issubst sbweak (ctxextend Γ A) Γ
-.
+Lemma SubstCons :
+  forall {σ u Γ Δ A},
+    istype Δ A ->
+    issubst σ Γ Δ ->
+    isterm Γ u A[σ] ->
+    issubst (u ⋅ σ) Γ (ctxextend Δ A).
+Proof.
+  intros σ u Γ Δ A hA hσ hu n B h.
+  induction n.
+  - rewrite_substs.
+    admit. (* Wrong?? *)
+  - rewrite_substs.
+    admit. (* This rule is probably wrong! *)
+Abort.
+
+Lemma SubstCtxConv :
+  forall {σ Γ Δ Δ'},
+    eqctx Δ' Δ ->
+    issubst σ Γ Δ ->
+    issubst σ Γ Δ'.
+Proof.
+  intros σ Γ Δ Δ' h1 h2 n B h.
+  ceapply TermCtxConv.
+  - apply h2.
+    ceapply TermCtxConv.
+    + eassumption.
+    + assumption.
+    + admit. (* Need precondition *)
+    + admit. (* Need precondition *)
+    + admit. (* Need precondition *)
+  - capply CtxRefl. admit. (* Need precondition *)
+  - admit. (* Need precondition *)
+  - admit. (* Need precondition *)
+  - admit. (* Precondition in the definition of issubst? *)
+Admitted.
 
 (* We show applying substitutions preserves typing. *)
 Lemma TySubst :
@@ -114,16 +154,22 @@ Proof.
   induction hA ; intros Γ σ hσ.
 
   (* TyCtxConv *)
-  - admit.
+  - { apply IHhA. ceapply SubstCtxConv.
+      - eassumption.
+      - assumption.
+    }
 
   (* TyProd *)
   - { rewrite_substs.
       capply TyProd.
-      - apply IHhA. apply SubstCons.
-        + admit. (* ok *)
-        + admit. (* not ok *)
-        + (* capply TermVarZero. *) (* not ok? :( *)
+      - apply IHhA. (* apply SubstCons. *)
+        (* We do it by hand, hopefully understanding who is SubstCons *)
+        intros [| n] C h.
+        + rewrite_substs.
+          (* Here we would need some kind of inversion AND induction
+             hypothesis. *)
           admit.
+        + rewrite_substs. admit.
       - apply X. assumption.
       - admit. (* ok *)
     }
