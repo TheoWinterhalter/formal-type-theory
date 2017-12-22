3444683f33405c762b8742e2b37b9692ed299aee
Théo Winterhalter <isheeft@gmail.com>
2017-12-22 14:40:38 +0100

    Hard to find a representation that works with sbweak and sbid

--- src/substitutions.v
+++ src/substitutions.v
@@ -66,6 +66,8 @@ Ltac rewrite_substs :=
 
 Section Substitutions.
 
+  Open Scope type_scope.
+
 (* Let's do it with preconditions for now *)
 (* Context `{configPrecondition : config.Precondition}. *)
 Local Instance havePrecondition : config.Precondition :=
@@ -82,6 +84,57 @@ Context `{configUnitType : config.UnitType}.
 Context `{configBoolType : config.BoolType}.
 Context `{configProdType : config.ProdType}.
 Context `{configSyntax : syntax.Syntax}.
+
+Fixpoint issubst_rec (σ : substitution) (Γ Δ : context) (hΔ : isctx Δ) : Type.
+  destruct hΔ as [| Δ A hΔ hA].
+  - exact Datatypes.unit.
+  - doConfig.
+    simple refine (_ * _).
+    + exact (issubst_rec σ↑ Γ Δ hΔ).
+    + exact (isterm Γ (var 0)[σ] A[σ↑]).
+Defined.
+
+Definition issubst σ Γ Δ := { h : isctx Δ & issubst_rec σ Γ Δ h }.
+
+Lemma SubstNil :
+  forall {σ Γ},
+    isctx Γ ->
+    issubst σ Γ ctxempty.
+Proof.
+  intros σ Γ hΓ.
+  exists CtxEmpty. exact tt.
+Defined.
+
+Lemma SubstCons :
+  forall {σ Γ Δ A},
+    isctx Γ ->
+    isctx Δ ->
+    istype Δ A ->
+    issubst (σ↑) Γ Δ ->
+    isterm Γ (var 0)[σ] A[σ↑] ->
+    issubst σ Γ (ctxextend Δ A).
+Proof.
+  intros σ Γ Δ A hΓ _ hA [hΔ hσ] hv.
+  unshelve eexists.
+  - now capply CtxExtend.
+  - cbn. split.
+    + assumption.
+    + assumption.
+Defined.
+
+Lemma SubstWeak :
+  forall {Γ A},
+    isctx Γ ->
+    istype Γ A ->
+    issubst sbweak (ctxextend Γ A) Γ.
+Proof.
+  intros Γ A hΓ hA.
+  exists hΓ. induction hΓ as [| Δ B hΔ ih hB].
+  - exact tt.
+  - split.
+    +
+Abort.
+
 
 (* A substitution is well-formed if it is well-formed on variables.
    The action on types and terms is derived from it.
@@ -100,14 +153,6 @@ Inductive issubst : forall (σ : substitution) (Γ : context), context -> Type :
       issubst (σ↑) Γ Δ ->
       isterm Γ (var 0)[σ] A[σ↑] ->
       issubst σ Γ (ctxextend Δ A)
-
-(* I'll take them as axioms for now.
-   Hopefully we can show later that these rules are subsumed.
- *)
-| SubstId :
-    forall {Γ},
-      isctx Γ ->
-      issubst sbid Γ Γ
 
 | SubstWeak :
     forall {Γ A},
@@ -147,19 +192,13 @@ Proof.
       * (* Some setoid rewrite might help me? *)
         (* It is true, but I'm lazy for now. *)
         admit.
-    + subst. (* What can we do now? *)
+    + subst. rename A0 into B.
 Abort. (* Maybe no longer necessary. *)
 
 Fact eqDropCons : forall {u σ}, (u ⋅ σ) ↑ ~ σ.
 Proof.
   intros u σ n.
   now rewrite_substs.
-Defined.
-
-Instance sbextSymmetric `{Syntax} : Symmetric sbextR.
-Proof.
-  intros σ ρ h n.
-  symmetry. apply h.
 Defined.
 
 Lemma SubstCons' :
@@ -174,215 +213,20 @@ Proof.
   intros Γ Δ A u σ hΓ hΔ hσ hA hu.
   apply SubstCons.
   - assumption.
-  - apply issubst_ext with (σ0 := σ).
-    + assumption.
-    + assumption.
-    + (* symmetry. *)
-      intro n. symmetry.
-      apply eqDropCons.
-    + assumption.
-  - rewrite_substs.
-    (* Some setoid rewrite would help us.
-       We'll only deal with those once we are convinced we're on the right
-       tracks.
-     *)
-    admit.
-Admitted.
-
-Fixpoint shiftby Δ (h : isctx Δ) (n : nat) : substitution.
-  destruct h.
-  - exact sbweak.
-  - doConfig. rename G into Δ.
-    exact (var n ⋅ shiftby Δ i (S n)).
-Defined.
-
-Require Import Coq.Vectors.Vector.
-
-Fixpoint ctxextension (Γ : context) {n : nat} (An : Vector.t type n) : context :=
-  match An with
-  | nil _ => Γ
-  | cons _ A n An => ctxextend (ctxextension Γ An) A
-  end.
-
-Fixpoint isctxextension (Γ : context) {n : nat} (An : Vector.t type n) :=
-  (isctx (ctxextension Γ An) *
-  match An with
-  | nil _ => isctx Γ
-  | cons _ A n An =>
-    isctx Γ * isctxextension Γ An
-  end)%type.
-
-Fact shiftbyWeak1 Γ h : shiftby Γ h (S 0) ~ sbweak.
-Proof.
-  intro m. destruct h.
-  - cbn. reflexivity.
-  - cbn. destruct m.
-    + rewrite_substs. reflexivity.
-    + rewrite_substs.
-Abort.
-
-Fixpoint shiftbyn Γ h n m : (var m)[shiftby Γ h n] = (var (n+m)).
-Proof.
-  destruct h.
-  - cbn. rewrite_substs.
-Abort.
-
-Fixpoint SubstShiftby Γ (h : isctx Γ) (n : nat) (An : Vector.t type n) :
-  isctxextension Γ An ->
-  issubst (shiftby Γ h n) (ctxextension Γ An) Γ.
-Proof.
-  intro h'.
-  config destruct h.
-  - apply SubstNil.
-  - simpl. apply SubstCons'.
-    + destruct An ; apply h'.
-    + config assumption.
-    + assert (ctxextension (ctxextend G A) An = ctxextension G (shiftin A An)).
-      { induction An.
-        - cbn. reflexivity.
-        - cbn. f_equal. apply IHAn. cbn in h'.
-          apply h'. (* Ugly because not a lemma. *)
-      }
-      rewrite H. apply SubstShiftby.
-      induction An.
-      * cbn in *. repeat split.
-        -- apply h'.
-        -- config assumption.
-        -- config assumption.
-        -- config assumption.
-      * cbn in *. repeat split.
-        -- rewrite <- H. apply h'.
-        -- config assumption.
-        -- apply IHAn.
-           ++ apply h'.
-           ++ admit. (* Injectivity of ctxextend again *)
-    + assumption.
-    + induction An.
-      * cbn in *. admit. (* Assuming it is indeed the weakening. *)
-      * cbn in h'. destruct h' as [h1 [h2 h3]].
-        specialize (IHAn h3).
-Abort.
-
-
-(* Fixpoint exsbid Γ (h : isctx Γ) : substitution *)
-
-(* with exsbweak Γ (A : type) (h : isctx Γ) : substitution. *)
-
-(*   - { destruct h. *)
-(*       - exact sbid. (* Anything would fit really. *) *)
-(*       - rename G into Γ. doConfig. *)
-(*         exact (var 0 ⋅ exsbweak Γ A i). *)
-(*     } *)
-
-(*   - { destruct h. *)
-(*       - exact sbweak. *)
-(*       - rename G into Γ, A0 into B. doConfig. *)
-
-(* Defined. *)
-
-(* Lemma SubstExId : *)
-(*   forall {Γ} (h : isctx Γ), *)
-(*     issubst (exsbid Γ h) Γ Γ. *)
-(* Proof. *)
-(*   intros Γ h. *)
-(*   induction h. *)
-(*   - apply SubstNil. *)
-(*   - rename G into Γ. doConfig. cbn. *)
-(*     apply SubstCons'. *)
-(*     + capply CtxExtend. *)
-(*       * capply i. (* Why didn't doConfig work here? *) *)
-(*       * assumption. *)
-(*     + capply i. *)
-(*     + specialize (X config.yes). *)
-
-
-Lemma SubstWeak :
-  forall {Γ A},
-    isctx Γ ->
-    istype Γ A ->
-    issubst sbweak (ctxextend Γ A) Γ.
-Proof.
-  intros Γ A hΓ.
-  induction hΓ ; intro hA.
-  - apply SubstNil.
-  - doConfig. rename A0 into B, G into Δ.
-    apply SubstCons.
-    + assumption.
-    + admit.
-    + (* rewrite_substs. ceapply TermVarSucc. *)
-      (* * capply CtxExtend ; assumption. *)
-      (* * *)
-Abort.
-
-Lemma SubstId :
-  forall {Γ},
-    isctx Γ ->
-    issubst sbid Γ Γ.
-Proof.
-  intros Γ hΓ. induction hΓ.
-  - apply SubstNil.
-  - apply SubstCons.
-    + assumption.
-    + doConfig. admit.
-    + doConfig. rewrite_substs. admit.
-Admitted. (* It holds as long as we can type weakening. *)
-
-Fixpoint dropCtx Δ (h : isctx Δ) (n : nat) : context.
-  destruct n.
-  - exact Δ.
-  - destruct h.
-    + exact ctxempty.
-    + doConfig. exact (dropCtx G i n).
-Defined.
-
-Fact dropCtx0 : forall Δ h, dropCtx Δ h 0 = Δ.
-Proof.
-  intros Δ h. destruct h ; reflexivity.
-Defined.
-
-Fixpoint SubstConsDrop {Γ Δ σ}
-         (hΓ : isctx Γ) (hΔ : isctx Δ) (hσ : issubst (σ↑) Γ (dropCtx Δ hΔ 1)) :
-  issubst σ Γ Δ.
-Proof.
-  destruct hΔ.
-  - apply SubstNil.
-  - rename G into Δ.
-    apply SubstCons.
-    + assumption.
-    + simpl in hσ. rewrite dropCtx0 in hσ. assumption.
-    + (* How should we take this into account? *)
-Abort.
-
-Fixpoint SubstDrop {Γ Δ σ n} (hΓ : isctx Γ) (hΔ : isctx Δ) :
-  issubst σ Γ Δ ->
-  issubst (σ ↑ n) Γ (dropCtx Δ hΔ n).
-Proof.
-  intro hσ.
-  destruct hΔ.
-  - simpl. apply SubstNil.
-  - rename G into Δ.
-    destruct n.
-    + simpl. admit. (* This should be assumed I guess. *)
-    + simpl.
-
-
-    + assumption.
-    +
-Abort.
-
-Lemma SubstCons :
-  forall {σ u Γ Δ A},
-    istype Δ A ->
-    issubst σ Γ Δ ->
-    isterm Γ u A[σ] ->
-    issubst (u ⋅ σ) Γ (ctxextend Δ A).
-Proof.
-  intros σ u Γ Δ A hA hσ hu n B h.
-  induction n.
-  - rewrite_substs.
-    admit. (* Wrong?? *)
-  - rewrite_substs.
-    admit. (* This rule is probably wrong! *)
+  - (* apply issubst_ext with (σ0 := σ). *)
+(*     + assumption. *)
+(*     + assumption. *)
+(*     + (* symmetry. *) *)
+(*       intro n. symmetry. *)
+(*       apply eqDropCons. *)
+(*     + assumption. *)
+(*   - rewrite_substs. *)
+(*     (* Some setoid rewrite would help us. *)
+(*        We'll only deal with those once we are convinced we're on the right *)
+(*        tracks. *)
+(*      *) *)
+(*     admit. *)
+(* Admitted. *)
 Abort.
 
 Lemma SubstCtxConv :
@@ -391,7 +235,10 @@ Lemma SubstCtxConv :
     issubst σ Γ Δ ->
     issubst σ Γ Δ'.
 Proof.
-  intros σ Γ Δ Δ' h1 h2 n B h.
+  intros σ Γ Δ Δ' h1 h2.
+  induction h2.
+  -
+
   ceapply TermCtxConv.
   - apply h2.
     ceapply TermCtxConv.
