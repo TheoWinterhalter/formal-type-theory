80a651f25af9c82ee8396dac110756d6b19a4ae7
Théo Winterhalter <isheeft@gmail.com>
2018-01-09 13:09:24 +0100

    Make substitution typing abstract

--- src/syntax.v
+++ src/syntax.v
@@ -12,8 +12,8 @@ Inductive level : Type :=
 Class CONS A B := _sbcons : A -> B -> B.
 Notation "u ⋅ σ" := (_sbcons u σ) (at level 20, right associativity).
 
-Class DROP substitution := _sbdrop : substitution -> substitution.
-Notation "σ ↑" := (_sbdrop σ) (at level 3).
+(* Class DROP substitution := _sbdrop : substitution -> substitution. *)
+(* Notation "σ ↑" := (_sbdrop σ) (at level 3). *)
 
 Class SUBST substitution tt :=
   _subst : tt -> substitution -> tt.
@@ -28,20 +28,17 @@ Class Syntax := {
   context : Type;
   type : Type;
   term : Type;
-  substitution : Type;
 
   ctxempty : context;
   ctxextend :> EXTEND context type;
 
   (* Some injectivity result seem necessary on contexts. *)
-  ctxextend_notempty :
-    forall {Γ A},
-      Γ, A = ctxempty -> False;
-  ctxextend_inj :
-    forall {Γ Δ A B},
-      Γ, A = Δ, B -> ((Γ = Δ) * (A = B))%type;
-
-  Subst :> SUBST substitution type;
+  (* ctxextend_notempty : *)
+  (*   forall {Γ A}, *)
+  (*     Γ, A = ctxempty -> False; *)
+  (* ctxextend_inj : *)
+  (*   forall {Γ Δ A B}, *)
+  (*     Γ, A = Δ, B -> ((Γ = Δ) * (A = B))%type; *)
 
   Prod : type -> type -> type;
   Id : type -> term -> term -> type;
@@ -51,9 +48,6 @@ Class Syntax := {
   BinaryProd : type -> type -> type;
   Uni : level -> type;
   El : level -> term -> type;
-
-
-  subst :> SUBST substitution term;
 
   var : nat -> term;
   lam : type -> type -> term -> term;
@@ -77,10 +71,15 @@ Class Syntax := {
   uniUni : level -> term;
 
   (* Substitutions *)
+  substitution : Type;
+
+  Subst :> SUBST substitution type;
+  subst :> SUBST substitution term;
+
   sbid : substitution;
   sbcons :> CONS term substitution;
   sbweak : substitution;
-  sbdrop :> DROP substitution;
+  (* sbdrop :> DROP substitution; *)
 
   (* Computation of substitutions *)
   sbidterm :
@@ -93,19 +92,19 @@ Class Syntax := {
     forall {σ u n}, (var (S n))[u ⋅ σ] = (var n)[σ];
   sbweakvar :
     forall {n}, (var n)[sbweak] = var (S n);
-  sbdropvar :
-    forall {σ n}, (var n)[σ↑] = (var (S n))[σ];
+  (* sbdropvar : *)
+  (*   forall {σ n}, (var n)[σ↑] = (var (S n))[σ]; *)
 
   (* Substitution extensionality principle *)
-  sbextR σ ρ := forall n, (var n)[σ] = (var n)[ρ];
-  sbext :
-    forall {σ ρ},
-      sbextR σ ρ ->
-      forall t, t[σ] = t[ρ];
-  Sbext :
-    forall {σ ρ},
-      sbextR σ ρ ->
-      forall T, T[σ] = T[ρ];
+  (* sbextR σ ρ := forall n, (var n)[σ] = (var n)[ρ]; *)
+  (* sbext : *)
+  (*   forall {σ ρ}, *)
+  (*     sbextR σ ρ -> *)
+  (*     forall t, t[σ] = t[ρ]; *)
+  (* Sbext : *)
+  (*   forall {σ ρ}, *)
+  (*     sbextR σ ρ -> *)
+  (*     forall T, T[σ] = T[ρ]; *)
 
   (* Action of substitutions *)
   SubstProd :
@@ -186,34 +185,74 @@ Class Syntax := {
   Arrow := fun (A B :  type) => Prod A B[sbweak]
 }.
 
+Class TypeTheory (S : Syntax) := {
+  isctx : context -> Type;
+  istype : context -> type -> Type;
+  isterm : context -> term -> type -> Type;
+  eqctx : context -> context -> Type;
+  eqtype : context -> type -> type -> Type;
+  eqterm : context -> term -> term -> type -> Type
+}.
+
+Class SubstitutionTyping (S : Syntax) (T : TypeTheory S) := {
+  (* Typing of substitutions *)
+  issubst : substitution -> context -> context -> Type;
+
+  SubstSbid : forall {Γ}, isctx Γ -> issubst sbid Γ Γ;
+  SubstWeak : forall {Γ A}, isctx Γ -> istype Γ A -> issubst sbweak (Γ,A) Γ;
+  SubstCtxConv :
+    forall {σ Γ Δ Δ'},
+      eqctx Δ' Δ ->
+      issubst σ Γ Δ ->
+      issubst σ Γ Δ';
+  SubstCons :
+    forall {σ u Γ Δ A},
+      issubst σ Γ Δ ->
+      istype Δ A ->
+      isterm Γ u A[σ] ->
+      issubst (u ⋅ σ) Γ (Δ, A);
+
+  TySubst :
+    forall {σ Γ Δ A},
+      issubst σ Γ Δ ->
+      istype Δ A ->
+      istype Γ A[σ];
+  TermSubst :
+    forall {σ Γ Δ A u},
+      issubst σ Γ Δ ->
+      istype Δ A ->
+      isterm Δ u A ->
+      isterm Γ u[σ] A[σ]
+}.
+
 End SyntaxDefinition.
 
 (* Notation "u ⋅ σ" := (sbcons u σ) (at level 20, right associativity). *)
 (* Notation "A [ σ ]" := (Subst A σ) (at level 0). *)
 (* Notation "t [ σ ]" := (subst t σ) (at level 0). *)
-Notation "σ ~ ρ" := (sbextR σ ρ) (at level 10).
+(* Notation "σ ~ ρ" := (sbextR σ ρ) (at level 10). *)
 
-Global Instance sbextRReflexive `{Syntax} : Reflexive sbextR.
-Proof.
-  intros σ n. reflexivity.
-Defined.
+(* Global Instance sbextRReflexive `{Syntax} : Reflexive sbextR. *)
+(* Proof. *)
+(*   intros σ n. reflexivity. *)
+(* Defined. *)
 
-Global Instance sbextRSymmetric `{Syntax} : Symmetric sbextR.
-Proof.
-  intros σ ρ h n.
-  symmetry. apply h.
-Defined.
+(* Global Instance sbextRSymmetric `{Syntax} : Symmetric sbextR. *)
+(* Proof. *)
+(*   intros σ ρ h n. *)
+(*   symmetry. apply h. *)
+(* Defined. *)
 
-Global Instance sbextRTransitive `{Syntax} : Transitive sbextR.
-Proof.
-  intros σ ρ θ h1 h2 n.
-  transitivity ((var n)[ρ]).
-  - apply h1.
-  - apply h2.
-Defined.
+(* Global Instance sbextRTransitive `{Syntax} : Transitive sbextR. *)
+(* Proof. *)
+(*   intros σ ρ θ h1 h2 n. *)
+(*   transitivity ((var n)[ρ]). *)
+(*   - apply h1. *)
+(*   - apply h2. *)
+(* Defined. *)
 
-Global Instance sbextREquivalence `{Syntax} : Equivalence sbextR.
-Proof.
-  split.
-  - intros σ. Fail reflexivity.
-Abort.
+(* Global Instance sbextREquivalence `{Syntax} : Equivalence sbextR. *)
+(* Proof. *)
+(*   split. *)
+(*   - intros σ. Fail reflexivity. *)
+(* Abort. *)
